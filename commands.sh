6517e109617353a65fac74afed3048121d47bc11
Ashwin Sankaralingam <ashwinoscar@gmail.com>
2019-04-24 12:15:01 -0600

    addedclass capabilities

---
+++ commands.sh
@@ -0,0 +1,7 @@
+#do these before you start 
+# build tf records and try
+# bash download_dataset.sh apple2orange
+# python3 build_data.py
+
+# currently we have added class conditions but training on two classes only
+python train.py
